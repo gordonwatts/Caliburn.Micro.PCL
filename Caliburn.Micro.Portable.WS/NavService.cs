4d3e83dcf6b7615f29fa6976922e7656dcb96b9f
Gordon Watts <gordon@gordonandpaula.net>
2013-12-30 22:49:30 -0500

    Added ease-of-use to create the nav service and declare it to the container.

--- Caliburn.Micro.Portable.WS/NavService.cs
+++ Caliburn.Micro.Portable.WS/NavService.cs
@@ -1,5 +1,4 @@
-﻿using Caliburn.Micro.Portable;
-
+﻿
 namespace Caliburn.Micro.Portable.WS
 {
     /// <summary>
@@ -9,9 +8,13 @@ namespace Caliburn.Micro.Portable.WS
     public class NavService : INavService
     {
         /// <summary>
-        /// Hold onto the navigation service
+        /// Local cache for use when we have to move around.
         /// </summary>
         private readonly INavigationService _nav;
+
+        /// <summary>
+        /// Hold onto the navigation service
+        /// </summary>
         public NavService(INavigationService nav)
         {
             _nav = nav;
@@ -20,10 +23,22 @@ namespace Caliburn.Micro.Portable.WS
         /// <summary>
         /// Move to the proper spot.
         /// </summary>
-        /// <typeparam name="T"></typeparam>
+        /// <typeparam name="T">Type of the view model to open</typeparam>
         public void DisplayViewModelFor<T>()
         {
             _nav.NavigateToViewModel<T>();
+        }
+
+        /// <summary>
+        /// Given a WinRT version of the navigation service, register this facade in the container
+        /// so others can make easy use of it.
+        /// </summary>
+        /// <param name="service">The WinRT Navigation Service</param>
+        /// <param name="container">The main container for CM</param>
+        public static void RegisterINavService(INavigationService service, WinRTContainer container)
+        {
+            var mynav = new NavService(service);
+            container.RegisterInstance(typeof(INavService), null, mynav);
         }
     }
 }
