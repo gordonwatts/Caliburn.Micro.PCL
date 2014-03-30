73a7fcf45bd28b94e573e47859e5f87ed2ec15da
Gordon Watts <gordon@gordonandpaula.net>
2014-03-30 03:20:12 -0700

    Add missing file that does the navigation service

---
+++ Caliburn.Micro.Portable.WP8/NavUriBuilder.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace Caliburn.Micro.Portable.WP8
+{
+    /// <summary>
+    /// Uri builder for the navigation service for Windows Phone
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    class NavUriBuilder<T> : INavUriBuilder<T>
+    {
+        private UriBuilder<T> _uriBuilder;
+
+        /// <summary>
+        /// Construct a nav service builder
+        /// </summary>
+        /// <param name="uriBuilder"></param>
+        public NavUriBuilder(UriBuilder<T> uriBuilder)
+        {
+            this._uriBuilder = uriBuilder;
+        }
+
+        /// <summary>
+        /// Cause the navigation to happen
+        /// </summary>
+        public void Navigate()
+        {
+            _uriBuilder.Navigate();
+        }
+
+        /// <summary>
+        /// Add a parameter onto the service
+        /// </summary>
+        /// <typeparam name="TValue"></typeparam>
+        /// <param name="accessor"></param>
+        /// <param name="val"></param>
+        /// <returns></returns>
+        public INavUriBuilder<T> WithParam<TValue>(System.Linq.Expressions.Expression<Func<T, TValue>> accessor, TValue val)
+        {
+            return new NavUriBuilder<T>(_uriBuilder.WithParam(accessor, val));
+        }
+    }
+}
